06784500eaf5bf19cb0f8cd6a6a7557cf7a78e85
timkeane <tkeane@doitt.nyc.gov>
2015-11-16 09:39:21 -0500

    make button color darker?

--- src/main/webapp/js/nyc/style.js
+++ src/main/webapp/js/nyc/style.js
@@ -53,7 +53,7 @@ nyc.Style = (function(){
 				this.facilityCache[zoom] = [new ol.style.Style({
 					image: new ol.style.Circle({
 						radius: radius,
-						fill: new ol.style.Fill({color: '#3b4c5e'}),
+						fill: new ol.style.Fill({color: '#181f4a'}),
 						stroke: new ol.style.Stroke({color: 'white', width: radius < 12 ? 1.5 : 2})
 					})
 				})];
